d369cf7b9f3d0049120b2fb6def0dec4ccf90251
jed1337 <j.caychingco@yahoo.com>
2020-07-12 12:25:47 +0800

    Proxy the Session Repository to SessionJpaRepository

--- conference-demo/src/main/java/com/pluralsight/conferencedemo/repositories/SpeakerJpaRepository.java
+++ conference-demo/src/main/java/com/pluralsight/conferencedemo/repositories/SpeakerJpaRepository.java
@@ -3,5 +3,8 @@ package com.pluralsight.conferencedemo.repositories;
 import com.pluralsight.conferencedemo.models.Speaker;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+//This doesn't have the @Repository annotation
+//On startup, Spring data is enabled and does a classpath search for interfaces or classes of type JpaRepository
+//If they exist, they are treated as repositories and stored in the Spring context that way
 public interface SpeakerJpaRepository extends JpaRepository<Speaker, Long> {
 }
